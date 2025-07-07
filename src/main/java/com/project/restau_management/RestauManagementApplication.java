4e882a2ce48e21112c061f1a3d79ba1211c3e87f
Yassine <119039473+ElouarnaziYassine@users.noreply.github.com>
2025-07-07 14:56:31 +0100

    Initial commit from Spring Initializr

---
+++ src/main/java/com/project/restau_management/RestauManagementApplication.java
@@ -0,0 +1,13 @@
+package com.project.restau_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestauManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestauManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/restau_management/RestauManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.restau_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestauManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
